74bb65148a0c920e72c0df8d9f31e34bb31bf137
Vivek Sharma <vivek6923sh@gmail.com>
2019-01-18 00:25:21 +0530

    Added client

---
+++ client/build/precache-manifest.57a324f69376fa8c53db011b90d6b2ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7cb627df9822aa5119a3",
+    "url": "/static/js/main.7cb627df.chunk.js"
+  },
+  {
+    "revision": "d7b98888702d505417bb",
+    "url": "/static/js/1.d7b98888.chunk.js"
+  },
+  {
+    "revision": "7cb627df9822aa5119a3",
+    "url": "/static/css/main.68619e72.chunk.css"
+  },
+  {
+    "revision": "6f2f6759bfd5bbae764d22f08950c825",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const config = require('./config/config');
+const path = require('path')
 
 var app = express()
-
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(bodyParser.json())
@@ -13,11 +14,11 @@ mongoose.connect(config.url) // Connecting to database
 .then(() => console.log("Connected to mongodb"))
 .catch(err => console.error("Error connecting to the mongodb", err))
 
-app.get( '/',(req,res) => {
-    res.send('Welcome to the playground app') // Serve a landing page here
-})
-
 require('./app/routes/auth.routes')(app) // We can also use the router provided by Express
 require('./app/routes/event.routes')(app)
+
+app.get( '*',(req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})
 
 app.listen(2000)
